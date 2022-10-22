56ee8e86c4f98871af37d7718948c940db805cd8
Adrien Saunier <adrien.saunier>
2022-10-22 23:03:03 +0200

    feat(): Renomme la page la plus aboutie en index, et conserve l ancienne

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="font-nunito bg-ocre">
-    <div class="grid md:grid-flow-col">
-      <Header />
-      <Separation class="bg-vert" />
-      <div class="p-8">
-        <DecouvrirRestaurant />
+  <div class="font-cantata bg-ocre text-vert">
+    <div class="flex flex-col-reverse md:flex-row border-solid border-t-8 border-white h-screen md:p-8">
+      <div class="basis-1/3 bg-cover bg-fond-jeanjean bg-right-top border-solid md:border-8 shadow-xl" />
+      <div class="basis-2/3 text-center p-8 grid gap-8 max-w-xl m-auto">
+        <img src="/photos/logo.png" alt="JeanJean" class="h-44 md:h-60 m-auto">
+        <h2 class="font-cantata font-semibold text-3xl tracking-wide leading-6">
+          découvrez un bistrot moderne, au cœur du quartier Garibaldi.
+        </h2>
+        <h3 class="font-medium text-2xl text-xl">
+          Avec un menu rythmé par les saisons, les plats sont conçus sous vos yeux à partir de produits bruts.
+        </h3>
         <HorairesOuverture />
+        <div>
+          Reserver
+          <a href="tel:+33102030405">Par téléphone</a>
+          <a href="mailto:xxxx@xx.xx">Par email</a>
+        </div>
       </div>
     </div>
   </div>
@@ -13,12 +23,10 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import Separation from '~/components/Molecule/Separation.vue'
-import DecouvrirRestaurant from '~/components/Molecule/DecouvrirRestaurant.vue'
 import HorairesOuverture from '~/components/Molecule/HorairesOuverture.vue'
 
 export default Vue.extend({
   name: 'IndexPage',
-  components: { HorairesOuverture, DecouvrirRestaurant, Separation }
+  components: { HorairesOuverture }
 })
 </script>
---
+++ pages/v0.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="font-nunito bg-ocre">
+    <div class="grid md:grid-flow-col">
+      <Header />
+      <Separation class="bg-vert" />
+      <div class="p-8">
+        <DecouvrirRestaurant />
+        <HorairesOuverture />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Separation from '~/components/Molecule/Separation.vue'
+import DecouvrirRestaurant from '~/components/Molecule/DecouvrirRestaurant.vue'
+import HorairesOuverture from '~/components/Molecule/HorairesOuverture.vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  components: { HorairesOuverture, DecouvrirRestaurant, Separation }
+})
+</script>
