c0799499b986136b674fbbace2cd4309f5fbd20c
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2023-05-11 20:12:22 +0200

    Update BoutonReservation.vue

--- components/Molecule/BoutonReservation.vue
+++ components/Molecule/BoutonReservation.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fixed md:relative bottom-0 left-0 right-0 text-center pb-8">
-    <a href="https://reservation.laddition.com/booking/jeanjeanrestaurant#/date" class="bg-brunFonce text-3xl px-8 py-2 text-white rounded-full drop-shadow-lg">Réserver</a>
+    <a href="https://bookings.zenchef.com/results?rid=361804&pid=1001" class="bg-brunFonce text-3xl px-8 py-2 text-white rounded-full drop-shadow-lg">Réserver</a>
   </div>
 </template>
 
